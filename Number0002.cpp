1323b46b8c17f5a79b3fc8aad257e0cf6f49836c
zere-debug <54212006+zere-debug@users.noreply.github.com>
2020-03-28 19:28:50 +0600

    https://acmp.ru/index.asp?main=task&id_task=2

---
+++ Number0002.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream> 
+using namespace std; 
+int main() {
+	long long n, s; 
+	cin >> n;
+
+	if (n > 0) {
+		s = (1 + n) * n / 2;
+	}
+
+	if (n < 0) {
+		s = (1 + n) * (2 - n) / 2;
+	}
+
+	if (n == 0) {
+		s = 1;
+	}
+
+	cout << s;
+
+	return 0;
+}
