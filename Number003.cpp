75c71d603b1557ebc000b539a400ded31c5ad197
zere-debug <54212006+zere-debug@users.noreply.github.com>
2020-04-04 21:49:33 +0600

    Add files via upload

---
+++ Number003.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int x, x1;
+	cin >> x;
+	x1 = x / 10;
+
+	if (x < 10) { cout << x * x; }
+
+	if (x >= 10)
+	{
+		cout << x1 * (x1 + 1) << "25";
+	}
+}
