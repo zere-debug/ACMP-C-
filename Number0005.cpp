ac11485e9a05ce742e94c76da696b8a1481f8a48
zere-debug <54212006+zere-debug@users.noreply.github.com>
2020-07-12 21:26:32 +0600

    Add files via upload

---
+++ Number0005.cpp
@@ -0,0 +1,39 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int t = 0, f = 0, n;
+
+	cin >> n;
+
+	int* a = new int[n];
+
+	for (int i = 0; i < n; i++) {
+
+		cin >> a[i];
+
+	}
+
+	for (int i = 0; i < n; i++) {
+
+		if (a[i] % 2 != 0) { cout << a[i] << " "; t++; }
+
+	}
+
+	cout << endl;
+
+	for (int i = 0; i < n; i++) {
+
+		if (a[i] % 2 == 0) { cout << a[i] << " "; f++; }
+
+	}
+
+	cout << endl;
+
+	if (t > f) { cout << "NO"; }
+
+	if (f > t || t == f) { cout << "YES"; }
+
+}
