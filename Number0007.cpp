b95432eea5c8822a402a39cfccd3856ca4eae7b4
zere-debug <54212006+zere-debug@users.noreply.github.com>
2020-07-13 23:01:50 +0600

    Add files via upload

---
+++ Number0007.cpp
@@ -0,0 +1,32 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+string max(string a, string b) {
+	int i = 0;
+	if (a.length() > b.length()) return a;
+	if (a.length() < b.length()) return b;
+
+	while (true) {
+		if (a[i] > b[i]) {
+			return a;
+			return 0;
+		}
+
+		if (a[i] < b[i]) {
+			return b;
+			return 0;
+		}
+		i++;
+	}
+	return a;
+
+}
+
+int main() {
+	string a, b, c;
+	cin >> a >> b >> c;
+	cout << max(a, max(b, c));
+	return 0;
+}
