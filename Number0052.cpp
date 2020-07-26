b701c47c4a0446527f0885d2b7d4609cf412b2bd
zere-debug <54212006+zere-debug@users.noreply.github.com>
2020-07-26 21:07:08 +0600

    Add files via upload

---
+++ Number0052.cpp
@@ -0,0 +1,11 @@
+﻿
+#include <iostream>
+using namespace std;
+int main() {
+    char ch;
+    int x;
+    cin >> ch >> x;
+    if ((ch == 'A' || ch == 'C' || ch == 'E' || ch == 'G') && (x % 2 == 1)) { cout << "BLACK"; }
+    else if ((ch == 'B' || ch == 'D' || ch == 'F' || ch == 'H') && (x % 2 == 0)) { cout << "BLACK"; }
+    else { cout << "WHITE"; }
+}
