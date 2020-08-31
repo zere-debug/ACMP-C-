9874cfbca3b66e56e0100c4ce36ab367cc26a72c
zere-debug <54212006+zere-debug@users.noreply.github.com>
2020-08-31 20:05:58 +0600

    https://acmp.ru/index.asp?main=task&id_task=8

---
+++ Number008.cpp
@@ -0,0 +1,14 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	long int x, y, z;
+	cin >> x >> y >> z;
+	
+	if (x * y == z) { cout << "YES"; }
+	else { cout << "NO"; }
+
+	return 0;
+}
