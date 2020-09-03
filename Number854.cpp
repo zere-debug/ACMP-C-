7340cf20d1350414a65476c66a2a603f287495b6
zere-debug <54212006+zere-debug@users.noreply.github.com>
2020-09-03 22:55:15 +0600

    Add files via upload

---
+++ Number854.cpp
@@ -0,0 +1,21 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main() {
+	string ch;
+	int a, b;
+	cin >> a >> b;
+	cin >> ch;
+
+    if (ch == "freeze") {
+        if (a < b) { cout << a; }
+        else cout << b;
+    }
+    if (ch == "heat") {
+        if (a > b) { cout << a; }
+        else cout << b;
+    }
+    if (ch == "fan") { cout << a; }
+    if (ch == "auto") { cout << b; }
+}
